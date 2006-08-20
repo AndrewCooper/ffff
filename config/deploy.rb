3b5ba56e156ecba26c77b967f920170735030a43
Andrew Cooper <andrew.cooper@hkcreations.org>
2006-08-20 23:55:54 +0000

    Removed the logos from version control. They will now sit at ~/ffff.hkcreations.org/logos Modified Capistrano's deploy config to set up the app for running on Dreamhost. Removed the .htaccess.production file since it didn't work.
    git-svn-id: file:///c/Projects/github/ffff/trunk@15 984be318-b012-0410-9d71-a3113208a5d2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,130 +1,76 @@
-# This defines a deployment "recipe" that you can feed to capistrano
+# Introductory SwitchTower config for Dreamhost.
+#
+# ORIGINAL BY: Jamis Buck
+#
+# Docs: http://manuals.rubyonrails.com/read/chapter/98
+#
+# HIGHLY MODIFIED BY: Geoffrey Grosenbach boss@topfunky.com
+#
+# Docs: http://nubyonrails.com/pages/shovel_dreamhost
+#
+# USE AT YOUR OWN RISK! THIS SCRIPT MODIFIES FILES, MAKES DIRECTORIES, AND STARTS
+# PROCESSES. FOR ADVANCED OR DARING USERS ONLY!
+#
+# DESCRIPTION
+#
+# This is a customized recipe for easily deploying web apps to a shared host.
+#
+# You also need to modify Apache's document root using Dreamhost's web control panel.
+#
+# For full details, see http://nubyonrails.com/pages/shovel_dreamhost
+#
+# To setup lighty, first edit this file for your primary Dreamhost account.
+#
+# Then run:
+#   rake remote:exec ACTION=setup
+#
+# This will create all the necessary directories for running Switchtower.
+#
+# From then, you can deploy your application with Switchtower's standard
+#   rake deploy
+#
+# Or rollback with
+#   rake rollback
+# 
+# This defines a deployment "recipe" that you can feed to switchtower
 # (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
 # (among other things) the deployment of your application.
-
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
-
-set :application, "ffff"
-set :repository, "http://svn.hkcreations.org/#{application}/trunk"
-
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
-
-role :web, "hkcreations.org"
-role :app, "hkcreations.org"
-role :db,  "hkcreations.org", :primary => true
-
-# =============================================================================
-# OPTIONAL VARIABLES
-# =============================================================================
-set :deploy_to, "/home/acooper/ffff.hkcreations.org" # defaults to "/u/apps/#{application}"
-# set :user, "flippy"            # defaults to the currently logged in user
-# set :scm, :darcs               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
-set :use_sudo, false
-set :checkout, "export"
-
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
-
-# =============================================================================
-# TASKS
-# =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
-
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
-task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { delete "/tmp/dump.sql" }
-
-  run "mysqldump -u theuser -p thedatabase > /tmp/dump.sql" do |ch, stream, out|
-    ch.send_data "thepassword\n" if out =~ /^Enter password:/
-  end
-end
-
-# Tasks may take advantage of several different helper methods to interact
-# with the remote server(s). These are:
 #
-# * run(command, options={}, &block): execute the given command on all servers
-#   associated with the current task, in parallel. The block, if given, should
-#   accept three parameters: the communication channel, a symbol identifying the
-#   type of stream (:err or :out), and the data. The block is invoked for all
-#   output from the command, allowing you to inspect output and act
-#   accordingly.
-# * sudo(command, options={}, &block): same as run, but it executes the command
-#   via sudo.
-# * delete(path, options={}): deletes the given file or directory from all
-#   associated servers. If :recursive => true is given in the options, the
-#   delete uses "rm -rf" instead of "rm -f".
-# * put(buffer, path, options={}): creates or overwrites a file at "path" on
-#   all associated servers, populating it with the contents of "buffer". You
-#   can specify :mode as an integer value, which will be used to set the mode
-#   on the file.
-# * render(template, options={}) or render(options={}): renders the given
-#   template and returns a string. Alternatively, if the :template key is given,
-#   it will be treated as the contents of the template to render. Any other keys
-#   are treated as local variables, which are made available to the (ERb)
-#   template.
+# NOTE: When editing on Windows, be sure to save with Unix line endings (LF).
 
-desc "Demonstrates the various helper methods available to recipes."
-task :helper_demo do
-  # "setup" is a standard task which sets up the directory structure on the
-  # remote servers. It is a good idea to run the "setup" task at least once
-  # at the beginning of your app's lifetime (it is non-destructive).
-  setup
+set :user, 'acooper'
+set :application, "ffff"
+set :repository, "file:///home/#{user}/svn.hkcreations.org/#{application}/trunk"
+# NOTE: If file:/// doesn't work for you, try this:
+#set :repository, "svn+ssh://home/#{user}/svn/#{application}"
 
-  buffer = render("maintenance.rhtml", :deadline => ENV['UNTIL'])
-  put buffer, "#{shared_path}/system/maintenance.html", :mode => 0644
-  sudo "killall -USR1 dispatch.fcgi"
-  run "#{release_path}/script/spin"
-  delete "#{shared_path}/system/maintenance.html"
+
+# =============================================================================
+# You shouldn't have to modify the rest of these
+# =============================================================================
+
+role :web, application
+role :app, application
+role :db,  application, :primary => true
+
+set :deploy_to, "/home/#{user}/#{application}.hkcreations.org"
+# set :svn, "/path/to/svn"       # defaults to searching the PATH
+set :use_sudo, false
+
+desc "Restart the FCGI processes on the app server as a regular user."
+task :restart, :roles => :app do
+  run "ruby #{current_path}/script/process/reaper --dispatcher=dispatch.fcgi"
 end
 
-# You can use "transaction" to indicate that if any of the tasks within it fail,
-# all should be rolled back (for each task that specifies an on_rollback
-# handler).
-
-desc "A task demonstrating the use of transactions."
-task :long_deploy do
-  transaction do
-    update_code
-    disable_web
-    symlink
-    migrate
-  end
-
-  restart
-  enable_web
-end
-
-desc "An after-deploy task to swap the production .htaccess file"
-task :after_deploy, :roles=>[:web,:app] do
-  run "mv public/.htaccess public/.htaccess.development"
-  run "mv public/.htaccess.production public/.htaccess"
-end
+# In config/deploy.rb
+desc "Setup the application to run in production on Dreamhost"
+task :after_symlink, :roles => [:web, :app] do
+	# Force production enviroment by replacing a line in environment.rb
+	run "perl -i -pe \"s/#ENV\\['RAILS_ENV'\\] \\|\\|= 'production'/ENV['RAILS_ENV'] ||= 'production'/\" #{current_path}/config/environment.rb"
   
+  # Make dispatcher executable
+  run "chmod a+x #{current_path}/public/dispatch.fcgi"
+  
+  # Link to the logos directory
+  run "ln -s #{deploy_to}/logos #{current_path}/public/logos"
+end
