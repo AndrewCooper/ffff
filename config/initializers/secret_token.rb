7de316c70ee26cf58ad63eb50499e2bbe0ff8cc0
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-05 00:21:42 -0500

    Upgrade to Rails 3.0.9
    Overwrote most standard files.
    Added initializers for time zone compensation and
    logo & backup directories.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FFFF::Application.config.secret_token = '456fc4274561c828b18e065743445afd887657f747a0ac2be4582d2ccd17c0d20f21ef8ffa01fb888b07906205c993c36c3f8596b1a803bac1111c3bc92fe31e'
