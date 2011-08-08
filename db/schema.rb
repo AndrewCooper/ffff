f755481a3c84f3deb8b5812295b8b9033d1b7aba
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-07 22:26:26 -0500

    Updated schema for new migration names.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 7) do
+ActiveRecord::Schema.define(:version => 20110807215727) do
 
   create_table "bowls", :force => true do |t|
     t.string  "name",       :default => "", :null => false
