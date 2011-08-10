a6f74c0dac71b8aaa3890c10d5c022d73ecd8a79
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-10 00:29:15 -0500

    Update Bowl controller to RESTful Rails3 style
    Rearranged methods into HTTP verbs
    Tweaked index to provide instance variables for _edit and _list
    Changed a link_to_remote

--- app/controllers/admin/bowl_controller.rb
+++ app/controllers/admin/bowl_controller.rb
@@ -1,9 +1,29 @@
 class Admin::BowlController < Admin::AdminController
-  def delete
-    Bowl.delete(params[:id])
-    render :nothing=>true
+
+  # GET /admin/bowl
+  def index
+    @title = "Administration :: Bowls"
+    @item = Bowl.new
+    @items = Bowl.find(:all, :order=>"name")
+    @games = Game.find_with_teamnames(:conditions=>"is_bowl = 1",:order=>"week,gametime").collect {|p| ["#{p.away_loc} #{p.away_name} @ #{p.home_loc} #{p.home_name}",p.id]}
   end
 
+  # GET /admin/bowl/new
+  def new
+    @item=Bowl.new
+    render :partial=>"admin/shared/newitem"
+  end
+
+  # POST /admin/bowl
+  def create
+  end
+
+  # GET /admin/bowl/:id
+  def show
+    @item = Bowl.find(params[:id])
+  end
+
+  # GET /admin/bowl/:id/edit
   def edit
     @colspan = 5
     if params[:id]
@@ -15,25 +35,7 @@ class Admin::BowlController < Admin::AdminController
     render :partial => "edit"
   end
 
-  def index
-    @title = "Administration :: Bowls"
-  end
-
-  def list
-    if params[:id]
-      @item = Bowl.find(params[:id])
-      #     @game = Game.find_with_teamnames(:conditions=>"games.id = #{@item.game_id}").first
-      render(:partial => "item") and return
-    else
-      @items = Bowl.find(:all, :order=>"name")
-    end
-  end
-
-  def new
-    @item=Bowl.new
-    render :partial=>"admin/shared/newitem"
-  end
-
+  # PUT /admin/bowl/:id
   def update
     if params[:id]
       @item = Bowl.find(params[:id])
@@ -50,6 +52,20 @@ class Admin::BowlController < Admin::AdminController
         logger.info "Errors: "+@item.errors.inspect
         render :text=>(@item.errors.inspect  + "\n")
       end
+    end
+  end
+
+  # DELETE /admin/bowl/:id
+  def delete
+    Bowl.delete(params[:id])
+    render :nothing=>true
+  end
+  
+  def list
+    if params[:id]
+      #     @game = Game.find_with_teamnames(:conditions=>"games.id = #{@item.game_id}").first
+      render(:partial => "item") and return
+    else
     end
   end
 end
