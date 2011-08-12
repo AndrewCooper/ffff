3d1c3f5541587f568803f7fffc6370bcc8e0667a
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-11 22:43:27 -0500

    Make Admin::GamesController RESTful
    Rearranged methods to REST actions
    Add route to new actions
    Created .rjs files for Rails3 javascript
    Cleaned up views.
    
    Change-Id: I7c890ca6c5a124cfd7a9bcd70c6e1a2dd7d9753f

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -1,55 +1,62 @@
 class Admin::GamesController < Admin::AdminController
-  def delete
-    Game.delete(params[:id])
-    render :nothing=>true
-  end
-
-  def edit
-    @colspan = 6
-    if params[:id]
-      @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
-    else
-      @item = Game.new
-      @item.gametime = current_app_time
-    end
-    @teams = Team.find_by_sql("SELECT location,name,id FROM teams ORDER BY location").collect{|t| [t.location+" "+t.name,t.id]}
-    render :partial => "edit"
-  end
-
+  # GET /admin/games
   def index
     @title = "Administration :: Games"
+    @item = Game.new
+    @items = Game.find_with_teamnames(:order=>"week,gametime")
+    @teams = Team.find_by_sql("SELECT location,name,id FROM teams ORDER BY location").collect{|t| [t.location+" "+t.name,t.id]}
   end
 
-  def list
-    if params[:id]
-      @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
-      render(:partial => "item") and return
-    else
-      @items = Game.find_with_teamnames(:order=>"week,gametime")
+  # GET /admin/games/:id
+  def show
+    @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
+    @teams = Team.find_by_sql("SELECT location,name,id FROM teams ORDER BY location").collect{|t| [t.location+" "+t.name,t.id]}
+  end
+
+  # GET /admin/games/new
+  def new
+    @item=Game.new
+  end
+
+  # GET /admin/games/:id/edit
+  def edit
+    @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
+    @teams = Team.find_by_sql("SELECT location,name,id FROM teams ORDER BY location").collect{|t| [t.location+" "+t.name,t.id]}
+  end
+
+  # POST /admin/games
+  def create
+    @item = Game.create( params[:item] )
+    @items = Game.find_with_teamnames(:order=>"week,gametime")
+    respond_to do |format|
+      format.html { redirect_to admin_games_path }
+      format.js
     end
   end
 
-  def new
-    @item=Game.new
-    render :partial=>"admin/shared/newitem"
-  end
-
+  # PUT /admin/games/:id
   def update
-    if params[:id]
-      @item = Game.find(params[:id])
-      if @item.update_attributes(params[:item])
-        redirect_to :action => "list",:id=>params[:id]
-      else
-        render :text=>(@item.errors.inspect  + "\n")
+    @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
+    if @item.update_attributes(params[:item])
+      respond_to do |format|
+        format.html { redirect_to admin_games_path }
+        format.js { render :action=>:show }
       end
     else
-      @item = Game.new(params[:item])
-      if @item.save
-        redirect_to :action => "list","component"=>true
-      else
-        logger.info "Errors: "+@item.errors.inspect
-        render :text=>(@item.errors.inspect  + "\n")
+      respond_to do |format|
+        format.html { redirect_to edit_admin_game_path(params[:id]) }
+        format.js { render :action=>:edit }
       end
+    end
+  end
+
+  # DELETE /admin/games/:id
+  def destroy
+    @item = Game.find_with_teamnames(:conditions=>"games.id = #{params[:id]}").first
+    @item.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_games_path }
+      format.js
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,9 @@ FFFF::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  namespace :admin do
+    resources :games
+  end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
