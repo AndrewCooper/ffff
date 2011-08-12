295c3e3d4b560984f550dabde1fa379a672b42f1
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-12 01:00:37 -0500

    Move image helper functions to AdminHelper

--- app/helpers/admin/games_helper.rb
+++ app/helpers/admin/games_helper.rb
@@ -1,10 +1,2 @@
 module Admin::GamesHelper
-
-  def edit_image( alt_txt )
-    image_tag( "edit_off.png", :alt=>alt_txt, :mouseover=>image_path("edit_on.png") )
-  end
-
-  def delete_image( alt_txt )
-    image_tag( "delete_off.png", :alt=>alt_txt, :mouseover=>image_path("delete_on.png") )
-  end
 end
