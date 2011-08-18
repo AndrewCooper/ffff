38bae77b2bab1d93f2f3366c9dbd4ad7e54de21f
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-18 15:50:42 -0500

    Remove comments from routes.rb
    Change-Id: I40a3bb5bd5b7d3de44ec49bd88220a2cc322d0b4

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 FFFF::Application.routes.draw do
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
   resource :user, :only => [:edit,:update] do
     member do
       get 'forgot_password' => :forgot_password
@@ -14,45 +7,7 @@ FFFF::Application.routes.draw do
     end
   end
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
   namespace :admin do
     resources :bowls
     resources :games
@@ -62,9 +17,6 @@ FFFF::Application.routes.draw do
     match "picks"=>"picks#index", :via=>:get
   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
   root :to => "score#index"
 
   # See how all your routes lay out with "rake routes"
