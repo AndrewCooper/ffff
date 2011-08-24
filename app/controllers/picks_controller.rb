ce7869598b3117abd7344b1044dd78064dc53e01
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-08-23 22:40:36 -0500

    Create pick objects upon edit request
    Creating picks when requested makes the view rendering and update
    processing much simpler.
    
    Change-Id: I01ddff8097968b16e9ca17f366e2735740983d21

--- app/controllers/picks_controller.rb
+++ app/controllers/picks_controller.rb
@@ -29,29 +29,28 @@ class PicksController < ApplicationController
 
     now = current_app_time
 
-    matches = {}
-    games = Game.where("gametime > ?",now).order("week,gametime").includes([:away_team,:home_team,:bowl])
-    games.each { |g| matches[g.id] = {:game=>g,:picks=>[]} }
-
-    picks = Pick.where("game_id IN (?) AND user_id = ?",matches.keys,@user.id)
-    picks.each { |p| matches[p.game_id][:picks].push(p) }
-
     @weeks = {}
-    matches.each do |gid,match|
-      week = match[:game].week
-      if @weeks[week].nil? then @weeks[week] = [] end
-      @weeks[week] << match
+    games = Game.where("gametime > ?",now).order("week,gametime").includes([:away_team,:home_team,:bowl])
+    Pick.transaction do
+      games.each do |g| 
+        week = g.week
+        if @weeks[week].nil?
+          @weeks[week] = []
+        end
+        match = {:game=>g,:picks=>[]}
+        match[:picks] << Pick.find_or_create_by_game_id_and_user_id( :game_id=>g.id, :user_id=>@user.id )
+        @weeks[week] << match
+      end
     end
   end
 
   # PUT /picks
   def update
     @now = current_app_time
-    created = 0
     updated = 0
     overdue = 0
+    # update existing picks
     Pick.transaction do
-      # update existing picks
       picks = params["picks"]
       unless picks.nil?
         picks.each do |pid,pick|
@@ -65,27 +64,9 @@ class PicksController < ApplicationController
           end
         end
       end
-
-      # create missing picks
-      picks = params["newpicks"]
-      unless picks.nil?
-        params["newpicks"].each do |pick|
-          game = Game.find(pick["game_id"])
-          if @now < game.gametime
-            if Pick.create(pick)
-              created += 1
-            end
-          else
-            overdue += 1
-          end
-        end
-      end
-    end
-    if created > 0
-      flash[:notice] = "#{created} new picks created."
     end
     if updated > 0
-      flash[:notice] = (flash.notice || "") + "#{updated} existing picks updated."
+      flash[:notice] = "#{updated} picks updated."
     end
     if overdue > 0
       flash[:warning] += "#{overdue} games have already started. These picks were not updated."
