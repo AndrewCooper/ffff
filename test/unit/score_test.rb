d929d25561b8cc4e8d61c312c31d4056c89da429
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-01 13:49:33 -0500

    Update Score unit test
    Change-Id: Id14595cea63b0dbf906db6d04804b1e4b6f30a4e

--- test/unit/score_test.rb
+++ test/unit/score_test.rb
@@ -1,14 +1,8 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require 'test_helper'
 
-class ScoreTest < Test::Unit::TestCase
-  fixtures :scores
-
-  def setup
-    @score = Score.find(1)
-  end
-
-  # Replace this with your real tests.
-  def test_truth
-    assert_kind_of Score,  @score
+class ScoreTest < ActiveSupport::TestCase
+  def test_exists
+    score = Score.first
+    assert_kind_of Score, score
   end
 end
