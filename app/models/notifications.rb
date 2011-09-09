2b43b5eab744a30bb39c9b0d9a5ee6d1bd1c4c6e
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-09 17:11:16 -0500

    Fix errors in Notifications unit test
    Fix deprecation warnings in Notifications model.
    Fix associations in games fixture.
    Fix errors in picks_alert notification view.
    
    Change-Id: I71373e9982714084d7c302b95e13885d38fa51a6

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -1,24 +1,26 @@
 class Notifications < ActionMailer::Base
 
   def signup( user )
+    @user       = user
     @subject    = 'Notifications#signup'
-    @body       = {:user => user}
     @recipients = user.email
     @from       = 'FFFF Administration <admin@hkcreations.org>'
     @sent_on    = Time.now
   end
 
   def forgot_password( user, new_pass )
+    @user       = user
+    @password   = new_pass
     @subject    = 'Forgotten Password for FFFF'
-    @body       = {:user => user, :password => new_pass}
     @recipients = user.email
     @from       = 'FFFF Administration <admin@hkcreations.org>'
     @sent_on    = Time.now
   end
 
   def picks_alert( user, games )
+    @user       = user
+    @games      = games
     @subject    = 'Pending Picks Alert'
-    @body       = {:user => user, :games => games}
     @recipients = user.email
     @from       = 'FFFF Administration <admin@hkcreations.org>'
     @sent_on    = Time.now
