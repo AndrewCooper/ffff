c883e6c883f341968850d96c092bedf25a666c7a
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-09 22:22:58 -0500

    Move team_logo helper function to ApplicationHelper
    Used in more places, this helper belongs to a broader scope.
    
    Change-Id: I0743cf7d5bc5e481b782ab9c68e65f0494db6dac

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,9 @@ module ApplicationHelper
     @_content_for[name] = ""
     content_for(name, content, &block)
   end
+
+  def team_logo( team )
+    image_tag "/logos/#{team.image}",{:id=>"image#{team.id}", :alt=>"#{team.location} #{team.name} Logo"}
+  end
 end
 
