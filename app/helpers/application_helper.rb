157123532e43da6864cb55c1e8a8e69b0ca80237
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-12 00:17:39 -0500

    Add options parameter to ApplicationHelper::team_logo
    Use for passing options to image_tag()
    
    Change-Id: I7edc262bd4d383db5f8bbbabe04e6f256dabc6ca

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,16 @@ module ApplicationHelper
     content_for(name, content, &block)
   end
 
-  def team_logo( team )
-    image_tag "/logos/#{team.image}",{:id=>"image#{team.id}", :alt=>"#{team.location} #{team.name} Logo"}
+  def team_logo( team, options={} )
+    if options[:id].nil?
+      options[:id] = "image#{team.id}"
+    end
+
+    if options[:alt].nil?
+      options[:alt] = "#{team.location} #{team.name} Logo"
+    end
+
+    image_tag "/logos/#{team.image}", options
   end
 end
 
