f42b625ab736b61da87a0868309299ce24b21ce6
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-14 10:02:37 -0500

    Update Pick unit test
    Test associations
    Test calculate_score()
    
    Change-Id: Ic2402eb290e833e8cb2c9e75a158eaeff700bbae

--- test/unit/pick_test.rb
+++ test/unit/pick_test.rb
@@ -1,36 +1,58 @@
 require 'test_helper'
 
 class PickTest < ActiveSupport::TestCase
-	
-  def test_relationships
-    #belongs_to :user
-    assert_equal( users(:usera).id, picks(:usera_first_pick).user_id )
-    assert_equal( users(:usera).id, picks(:usera_first_pick).user.id )
-
-    #belongs_to :game
-    assert_equal( games(:pokesVSsooners).id, picks(:usera_first_pick).game_id )
-    assert_equal( games(:pokesVSsooners).id, picks(:usera_first_pick).game.id )
+  def setup
+    @pick = picks(:usera_first_pick)
+    @user = users(:usera)
+    @game = games(:pokesVSsooners)
   end
-	
-  def test_validations
-    # validates_numericality_of :home_score, :integer_only=>true, :greater_than_or_equal_to=>0
-    # validates_numericality_of :away_score, :integer_only=>true, :greater_than_or_equal_to=>0
-    # validates_numericality_of :pick_score, :integer_only=>true, :greater_than_or_equal_to=>0
 
-    p = picks( :usera_first_pick )
-    p["home_score"] = -1           # Negative integer
-    assert( !p.valid? )
-    p["home_score"] = 50           # Positive integer
-    assert( p.valid? )
+  test "belongs_to :user" do
+    assert_not_nil @pick.user
+    assert_kind_of User, @pick.user
+    assert_equal @user, @pick.user
+  end
 
-    p["away_score"] = -1           # Negative integer
-    assert( !p.valid? )
-    p["away_score"] = 50           # Positive integer
-    assert( p.valid? )
+  test "belongs_to :game" do
+    assert_not_nil @pick.game
+    assert_kind_of Game, @pick.game
+    assert_equal @game, @pick.game
+  end
 
-    p["pick_score"] = -1           # Negative integer
+  test "validates_numericality_of :home_score, :integer_only=>true, :greater_than_or_equal_to=>0" do
+    p = Pick.new
+    p.home_score = -1           # Negative integer
     assert( !p.valid? )
-    p["pick_score"] = 50           # Positive integer
+    p.home_score = 50           # Positive integer
     assert( p.valid? )
+  end
+
+  test "validates_numericality_of :away_score, :integer_only=>true, :greater_than_or_equal_to=>0" do
+    p = Pick.new
+    p.away_score = -1           # Negative integer
+    assert( !p.valid? )
+    p.away_score = 50           # Positive integer
+    assert( p.valid? )
+  end
+
+  test "validates_numericality_of :pick_score, :integer_only=>true, :greater_than_or_equal_to=>0, :allow_nil=>true" do
+    p = Pick.new
+    p.pick_score = -1           # Negative integer
+    assert( !p.valid? )
+    p.pick_score = nil          # nil
+    assert( p.valid? )
+    p.pick_score = 50           # Positive integer
+    assert( p.valid? )
+  end
+
+  test "calculate_score" do
+    p = Pick.new( :home_score=>10, :away_score=>20 )
+    assert_equal  0, p.calculate_score( 10, 20 )      # perfect pick
+    assert_equal 10, p.calculate_score(  5, 20 )      # away_score >
+    assert_equal 10, p.calculate_score( 15, 20 )      # away_score <
+    assert_equal 10, p.calculate_score( 10, 25 )      # home_score >
+    assert_equal 10, p.calculate_score( 10, 15 )      # home_score <
+    assert_equal 10, p.calculate_score( 10, 15 )      # spread >
+    assert_equal 10, p.calculate_score( 10, 25 )      # spread <
   end
 end
