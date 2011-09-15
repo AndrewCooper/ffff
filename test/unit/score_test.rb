f6584a89149637bb537548e716d7e8fa2a6124d3
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-15 09:11:56 -0500

    Update Score unit test
    Test associations
    Test user_stats
    Add User and Score fixtures to more fully exercise user_stats()
    
    Change-Id: I0b3844d5e40ab87a5e448e9f861f677fc52027d6

--- test/unit/score_test.rb
+++ test/unit/score_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class ScoreTest < ActiveSupport::TestCase
-  def test_exists
-    score = Score.first
-    assert_kind_of Score, score
+  def setup
+    @user = users(:usera)
+    @score = scores(:usera_score1)
+  end
+
+  test "belongs_to :user" do
+    assert_not_nil @score.user
+    assert_kind_of User, @score.user
+    assert_equal @user, @score.user
+  end
+
+  test "user_stats" do
+    stats = Score.user_stats( users(:usera).id )
+    assert_equal 2, stats[:rank]
+    assert_equal 8, stats[:score]
+    assert_equal 0, stats[:ties].count
   end
 end
