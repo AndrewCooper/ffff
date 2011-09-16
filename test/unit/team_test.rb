27cb45e8984d8c49854a2ec77d2feb2b30ccca63
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-15 21:17:22 -0500

    Update Team unit test
    Test associations
    Test fullname()
    
    Change-Id: I2f5ff84186180a209a0f1d24c6552f96ec32f5e9

--- test/unit/team_test.rb
+++ test/unit/team_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  def test_exists
-    team = Team.first
-    assert_kind_of Team, team
+  def setup
+    @team = teams(:wildcats)
+    @away_game = games(:wildcatsVSjayhawks)
+    @home_game = games(:pokesVSwildcats)
+  end
+
+  test 'has_many :away_games, :class_name => "Game", :foreign_key => "away_team_id"' do
+    assert_not_nil @team
+    assert_not_nil @team.away_games
+    assert_kind_of Array, @team.away_games
+    assert_equal 1, @team.away_games.all.count
+    assert_equal 1, @team.away_games.all.count( @away_game )
+  end
+
+  test 'has_many :home_games, :class_name => "Game", :foreign_key => "home_team_id"' do
+    assert_not_nil @team
+    assert_not_nil @team.home_games
+    assert_kind_of Array, @team.home_games
+    assert_equal 1, @team.home_games.all.count
+    assert_equal 1, @team.home_games.all.count( @home_game )
+  end
+
+  test "fullname" do
+    assert_equal @team.location + " " + @team.name, @team.fullname
   end
 end
