dd1000c126ee2543885430e93be58c3c320ac0e1
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-17 09:33:15 -0500

    Update User unit test
    Test associations
    Test validations
    Test class and instance methods
    Update scores fixture for User.rankings
    
    Change-Id: I243115035958ca4f5c4d89c6316f19cad19a31d8

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,82 +1,103 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_empty_password
-    oldpass = users(:usera).password.clone
-    users(:usera).password = ""
-    users(:usera).save
+  def setup
+    @user = users(:usera)
+  end
+
+  test 'empty password does not modify saved password' do
+    oldpass = @user.password.clone
+    @user.password = ""
+    @user.save
     assert_equal 0, users(:usera).errors.count
 
-    user = User.find(users(:usera).id)
+    user = User.find(@user.id)
     assert_equal oldpass, user.password
   end
 
-  def test_name
-    u = users(:usera)
-    assert_equal( "#{u.firstname} #{u.lastname}", u.name )
+  test 'class method ranked' do
+    @scores = User.ranked.all
+    assert_equal users(:userb), @scores.first
+    assert_equal users(:usera), @scores.second
+    assert_equal 9, @scores.second.wins
+    assert_equal 4, @scores.second.closests
+    assert_equal 1, @scores.second.sevens
+    assert_equal 1, @scores.second.perfects
+    assert_equal 8, @scores.second.total
+    assert_equal users(:userc), @scores.third
   end
 
-  def test_session_info
-    u = users(:usera)
-    session_hash = { :name=>"#{u.firstname} #{u.lastname}", :uid=>u.id, :admin=>u.is_admin }
-    assert_equal( session_hash, u.session_info )
+  test 'instance method name' do
+    assert_equal @user.firstname + " " + @user.lastname, @user.name
   end
 
-  def test_relationships_picks
-    # has_many :picks, :dependent => :delete_all
-    u = users(:usera)
-    assert_equal( 2, u.picks.count )
-    pick_ids = u.picks.collect{ |p| p.user_id }
-    assert_equal( [u.id]*2, pick_ids )
-    assert_equal( [games(:pokesVSsooners).id, games(:wildcatsVSjayhawks).id].sort,
-      u.picks.collect { |p| p.game_id }.sort )
-    User.destroy(u)
+  test 'instance method session_info' do
+    session_hash = { :name=>"#{@user.firstname} #{@user.lastname}", :uid=>@user.id, :admin=>@user.is_admin }
+    assert_equal session_hash, @user.session_info
+  end
+
+  test 'has_many :picks, :dependent => :delete_all' do
+    assert_equal 2, @user.picks.count
+    pick_ids = @user.picks.collect{ |p| p.user_id }
+    assert_equal [@user.id]*2, pick_ids
+    assert_equal [games(:pokesVSsooners).id, games(:wildcatsVSjayhawks).id].sort, @user.picks.collect { |p| p.game_id }.sort
+    User.destroy(@user)
     assert_raise ActiveRecord::RecordNotFound do
       Pick.find(pick_ids)
     end
   end
 
-  def test_relationship_scores
-    # has_many :scores, :dependent => :delete_all
-    u = users(:usera)
-    assert_equal( 2, u.scores.count )
-    score_ids = u.scores.collect{ |s| s.user_id }
-    assert_equal( [u.id]*2, score_ids )
-    assert_equal( [scores(:usera_score1).week, scores(:usera_score2).week].sort,
-      u.scores.collect { |s| s.week }.sort )
-    User.destroy(u)
+  test 'has_many :scores, :dependent => :delete_all' do
+    assert_equal 2, @user.scores.count
+    score_ids = @user.scores.collect{ |s| s.user_id }
+    assert_equal [@user.id]*2, score_ids
+    assert_equal [scores(:usera_score1).week, scores(:usera_score2).week].sort, @user.scores.collect { |s| s.week }.sort
+    User.destroy(@user)
     assert_raise ActiveRecord::RecordNotFound do
       Score.find( score_ids )
     end
   end
 
-  def test_validation_login
-    user = users(:usera).clone
-    assert(!user.valid?)
-    assert(user.errors[:login].any?, "Expected an error after validation")
-
-    user.login = "12"
-    user.save
-    assert_equal 1, user.errors.count
-
-    user.login = "l"*41
-    user.save
-    assert_equal 1, user.errors.count
+  test 'validates_uniqueness_of :login, :message => "NOT A UNIQUE LOGIN"' do
+    userclone = users(:usera).clone
+    assert !userclone.valid?
+    assert userclone.errors[:login].any?
   end
 
-  def test_validation_presence
-    user = User.new
-    user.password = '1'
-    user.save
-    assert_equal 3, user.errors.count, user.errors.to_xml
+  test 'validates_length_of :login, :within => 3..40' do
+    @user.login = "l"*2
+    assert !@user.valid?
 
-    user.login = "abc"
-    user.password = '1';
-    user.save
-    assert_equal 1, user.errors.count, user.errors.to_xml
+    @user.login = "l"*41
+    assert !@user.valid?
 
-    user.password = "12345"
-    user.save
-    assert_equal 0, user.errors.count, user.errors.to_xml
+    @user.login = "l"*3
+    assert @user.valid?
+
+    @user.login = "l"*40
+    assert @user.valid?
+  end
+
+  test 'validates_length_of :password, :within => 5..40' do
+    @user.password = "p"*4
+    assert !@user.valid?
+
+    @user.password = "p"*41
+    assert !@user.valid?
+
+    @user.password = "p"*5
+    assert @user.valid?
+
+    @user.password = "p"*40
+    assert @user.valid?
+  end
+
+  test 'validates_presence_of :login, :password' do
+    @user = User.new
+    @user.password = "p"*5
+    assert !@user.valid?
+
+    @user.login = "l"*3
+    assert @user.valid?
   end
 end
