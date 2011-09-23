38a05d91cbfa67eea3971145da6cf7d0bb8456d3
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-23 09:23:45 -0500

    Rename UserControllerTest to UsersControllerTest
    Removed trailing whitespace
    
    Change-Id: I0d90f7222a24185169aa8f5e5587c8b98adc1c57

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,14 +1,12 @@
 require 'test_helper'
 
-class UserControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionController::TestCase
   def setup
   end
 
   def test_forgot_password_success
     get :forgot_password
     assert_response :success
-		
     post :forgot_password, :username => users(:usera).login
-		
   end
 end
