e0bb3ace71381e21792d1cff3932b4e4efd99129
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-23 08:10:40 -0500

    Update LoginController functional test
    Test logging in and redirecting to /user/edit for changing password.
    
    Change-Id: I4ebfe7fa27cca214b97fade0a4c2ae05525ffe76

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
+  include LoginResponseCalculation
+
   test 'should get login as html' do
     get :request_login
     assert_response :redirect
@@ -11,5 +13,15 @@ class LoginControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil session[:challenge]
     assert_nil     session[:user]
+  end
+
+  test 'login usera successfully, change password' do
+    get :request_login, {:format=>:js}
+    user = users(:usera)
+    params = {:login => user.login, :response => calculate_login_response( user.password, session[:challenge] ) }
+    post :login, params
+    assert_response :redirect
+    assert_redirected_to edit_user_path
+    assert_equal "You are now encouraged to change your password.", flash[:notice]
   end
 end
