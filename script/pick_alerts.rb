f4670b82b30a90f091f2f107c7de40b494370134
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-09-23 20:57:54 -0500

    Encapsulate PickAlerts into class
    Loading the class as library makes it easier to test.
    
    Change-Id: Ibfec652ccdd4b9eb5e1b835f0ab396ad8986d732

--- script/pick_alerts.rb
+++ script/pick_alerts.rb
@@ -1,16 +1,25 @@
 #!/usr/bin/env rails runner
 
-games = Game.includes(:away_team,:home_team).where("games.gametime >= ? AND games.gametime < ?",Time.now,Time.now.tomorrow)
-User.all.each do |user|
-  if !games.empty? then
-    crit_games = []
-    games.each do |game|
-      if game.picks.where(:user_id=>user).empty?
-        crit_games.push game
+class PickAlertsRunner
+  def run
+    games = Game.includes(:away_team,:home_team).where("games.gametime >= ? AND games.gametime < ?",Time.now,Time.now.tomorrow)
+    User.all.each do |user|
+      if !games.empty? then
+        crit_games = []
+        games.each do |game|
+          if game.picks.where(:user_id=>user).empty?
+            crit_games.push game
+          end
+        end
+        if !crit_games.empty? and user.alerts? then
+          Notifications.picks_alert(user,crit_games).deliver
+        end
       end
     end
-    if !crit_games.empty? and user.alerts? then
-      Notifications.deliver_picks_alert(user,crit_games)
-    end
   end
+end
+
+if __FILE__ == $0
+  x = PickAlertsRunner.new
+  x.run
 end
