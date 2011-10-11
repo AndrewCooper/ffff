d2b27b72a16d70c53ecb1c1446cf28c2afe4498c
Andrew Cooper <andrew.cooper@hkcreations.org>
2011-10-11 13:46:02 -0500

    Add functional tests for Admin::ScoreController
    Moved bulk of score calculation logic to lib/score_calculation.rb. It
    will be unit tested separately.

--- test/functional/admin/score_controller_test.rb
+++ test/functional/admin/score_controller_test.rb
@@ -2,10 +2,19 @@ require 'test_helper'
 
 class Admin::ScoreControllerTest < ActionController::TestCase
   def setup
+    @session = { :user=>users(:admin).session_info }
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  test 'should get index' do
+    get :index, nil, @session
+    assert_response :success
+    assert_equal "Administration :: Scores", assigns[:title]
+  end
+
+  test 'should post calculate' do
+    post :calculate, nil, @session
+    assert_response :success
+    assert_equal "Administration :: Calculation Results", assigns[:title]
+    assert_not_nil assigns[:scores]
   end
 end
