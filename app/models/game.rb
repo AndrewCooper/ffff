5a94308cd379cc41a8b38f1908d8c0fc19080d32
Andrew Cooper <me@andrewcooper.me>
2012-08-23 22:51:11 -0500

    Allow nil for Game.home_score and Game.away_score
    Games are created with nil scores to allow them to be skipped during score
    calculation until the game's final score can be input.
    
    Change-Id: I6ccf20ecf23276f8e7630dc12dfa961414c87086

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,8 +5,8 @@ class Game < ActiveRecord::Base
   belongs_to :home_team, :class_name=>"Team", :foreign_key=>"home_team_id"
 
   attr_accessible :away_score, :away_team, :away_team_id, :gametime, :home_score, :home_team, :home_team_id, :is_bowl, :week
-  validates :away_score, :numericality => { :only_integer=>:true, :greater_than_or_equal_to=>0 }
-  validates :home_score, :numericality => { :only_integer=>:true, :greater_than_or_equal_to=>0 }
+  validates :away_score, :numericality => { :only_integer=>:true, :greater_than_or_equal_to=>0, :allow_nil=>:true }
+  validates :home_score, :numericality => { :only_integer=>:true, :greater_than_or_equal_to=>0, :allow_nil=>:true }
   validates :week,       :numericality => { :only_integer=>:true, :greater_than_or_equal_to=>1 }
   validates :is_bowl, :inclusion => { :in => [0,1] }
 
